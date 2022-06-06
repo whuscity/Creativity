87c9138583a6ee373ad09a87c647797f6ed9621d
pyr1009 <panyiru8012@163.com>
2022-06-06 18:13:12 +0800

    初始化

---
+++ src/main/java/cn/edu/whu/lilab/creativity/CreativityApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.whu.lilab.creativity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreativityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreativityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/whu/lilab/creativity/CreativityApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.whu.lilab.creativity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreativityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
