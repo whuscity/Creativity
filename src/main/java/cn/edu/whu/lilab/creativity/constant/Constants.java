7de4f9444306640518b1abc722ca5f39b59813df
pyr1009 <panyiru8012@163.com>
2022-06-26 17:17:51 +0800

    增加常量类、公共返回对象类

---
+++ src/main/java/cn/edu/whu/lilab/creativity/constant/Constants.java
@@ -0,0 +1,17 @@
+package cn.edu.whu.lilab.creativity.constant;
+
+/**
+ * 常量
+ */
+public final class Constants {
+
+    /**
+     * 调用成功 ResponseCode
+     */
+    public static final int SUCCESS = 200;
+
+    /**
+     * 调用失败 ResponseCode
+     */
+    public static final int FAIL = 500;
+}
---
+++ src/main/java/cn/edu/whu/lilab/creativity/controller/AbstractController.java
@@ -0,0 +1,13 @@
+package cn.edu.whu.lilab.creativity.controller;
+
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "摘要API")
+@RestController
+public class AbstractController {
+
+    // public R<>
+}
