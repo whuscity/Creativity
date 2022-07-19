4802170ff434d3ddd4893bf13db97b026000f217
pyr1009 <panyiru8012@163.com>
2022-07-19 20:26:23 +0800

    解决跨域问题

--- src/main/java/cn/edu/whu/lilab/creativity/controller/CiteController.java
+++ src/main/java/cn/edu/whu/lilab/creativity/controller/CiteController.java
@@ -11,6 +11,7 @@ import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import springfox.documentation.annotations.ApiIgnore;
 
 @Api(tags = "参考及引证API")
 @RestController
+@CrossOrigin()
 public class CiteController {
 
     @Autowired
--- src/main/java/cn/edu/whu/lilab/creativity/controller/SearchController.java
+++ src/main/java/cn/edu/whu/lilab/creativity/controller/SearchController.java
@@ -14,6 +14,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
@@ -22,6 +23,7 @@ import java.util.regex.Pattern;
 
 @Api(tags = "搜索API")
 @RestController
+@CrossOrigin()
 public class SearchController {
 
     @Autowired
